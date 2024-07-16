a91397cade3bf995c9133bf6593fad819e545d99
rgd1212 <rodigonzalesdolz@gmail.com>
2024-07-16 18:11:33 -0400

    inciando proyecto

---
+++ src/test/java/com/aluracursos/challengeLiteratura/ChallengeLiteraturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluracursos.challengeLiteratura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeLiteraturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
